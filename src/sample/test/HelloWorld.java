0091fc708e134d6c61ef90ae2170150f4f62ad04
vinay <vinaysai781@gmail.com>
2022-09-16 16:27:31 +0530

    first commit

---
+++ src/sample/test/HelloWorld.java
@@ -0,0 +1,28 @@
+package sample.test;//packages name
+
+public class HelloWorld {// class decleration
+
+	public static void main(String[] args) {// main method
+		System.out.println("Welcome to Java");//output statement
+		System.out.println("Hello World!");
+		
+		int firstNumber=30;
+		char myVariable='C';
+		float price=70.24f;
+		double totalPrice=888.222d;
+		String str="java full stack";
+		boolean status=true;
+		
+		
+		System.out.println(firstNumber);
+		System.out.println(status);
+		System.out.println(totalPrice);
+		System.out.println(price);
+		System.out.println(myVariable);
+		System.out.println(str);
+		
+		
+		
+	}
+
+}
